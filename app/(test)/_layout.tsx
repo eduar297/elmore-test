37607b3a51ebe9f6bc28d37c37c5f7ca9e2af25b
eduar2 <eduar2.moreira@gmail.com>
2026-03-23 18:45:35 -0300

    Refactor layout structure by replacing Tabs with Stack and removing unused Android and iOS screens

--- app/(test)/_layout.tsx
+++ app/(test)/_layout.tsx
@@ -1,49 +1,15 @@
-import { Tabs } from "expo-router";
+import { Stack } from "expo-router";
 import React from "react";
 
-import { HapticTab } from "@/components/haptic-tab";
-import { IconSymbol } from "@/components/ui/icon-symbol";
-import { Colors } from "@/constants/theme";
-import { useColorScheme } from "@/hooks/use-color-scheme";
-
 export default function TestLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}
-    >
-      <Tabs.Screen
+    <Stack>
+      <Stack.Screen
         name="index"
         options={{
-          title: "Index",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
-          ),
+          headerShown: false,
         }}
       />
-      <Tabs.Screen
-        name="ios"
-        options={{
-          title: "iOS",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="flask.fill" color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="android"
-        options={{
-          title: "Android",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="flask.fill" color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+    </Stack>
   );
 }
